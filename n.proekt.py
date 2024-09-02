64c34806c25fa5e9f759e5ee3363086d8b429312
sanek-pivo <kapelka_555@mail.ru>
2024-09-02 23:37:01 +0700

    Add files via upload

---
+++ n.proekt.py
@@ -0,0 +1,5 @@
+first_name ="курс:Python"
+apple_count='всего задач:12'
+second_name='затрачено часов:1.5'
+orange_count='среднее время выполнения 0.125 часа'
+print(first_name,',',second_name,',',apple_count,',',orange_count)
